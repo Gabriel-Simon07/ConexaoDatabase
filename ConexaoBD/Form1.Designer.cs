e6e5e36180eeeff034a94ca4c75460f430a4898a
Gabriel-Simon07 <gabrielsimon775@gmail.com>
2021-03-23 17:29:21 -0300

    Estrutura básica para conexão com Banco de Dados

---
+++ ConexaoBD/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ConexaoBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBanco = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBanco
+            // 
+            this.btnBanco.Location = new System.Drawing.Point(347, 216);
+            this.btnBanco.Name = "btnBanco";
+            this.btnBanco.Size = new System.Drawing.Size(164, 74);
+            this.btnBanco.TabIndex = 0;
+            this.btnBanco.Text = "Manda Banco";
+            this.btnBanco.UseVisualStyleBackColor = true;
+            this.btnBanco.Click += new System.EventHandler(this.btnBanco_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBanco);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBanco;
+    }
+}
+
---
+++ ConexaoBD/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexaoBD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBanco_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connetionString;
+                SqlConnection cnn;
+                connetionString = @"Data Source=127.0.0.1;Initial Catalog=testebd_c;";
+                cnn = new SqlConnection(connetionString);
+                cnn.Open();
+                MessageBox.Show("Salvo com sucesso");
+                cnn.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Não está funcionando"+ex);
+            }
+        }
+    }
+}
